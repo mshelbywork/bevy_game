182e9304224c3ec4e6afec7e487213790b8fc871
user <mshelbywork@gmail.com>
2026-08-11 14:30:17 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use bevy::prelude::*;
+
+fn main () {
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_systems(Startup, setup)
+    .run();
+}
+
+fn setup (mut commands: Commands) {
+    commands.spawn (Camera2d);
+
+    commands.spawn ((
+        Text2d::new("@"),
+        TextFont {
+            font_size: FontSize::Px(12.0),
+            font: default(),
+            ..default()
+        },
+        TextColor (Color::WHITE), 
+        Transform::from_translation (Vec3::ZERO),
+        Player
+    ));
+    
+}
+
+#[derive (Component)]
+struct Player ;
